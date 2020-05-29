368a67c9f118e7f9e55af5c99d69865e1c9ac7bd
Regis Zanandrea <regiszanandrea@gmail.com>
2020-05-29 18:55:51 -0300

    add Cache entity

---
+++ app/Entities/Cache.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Cache
+ * @package App\Entities
+ */
+class Cache extends Model
+{
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'key';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'cache';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'key', 'value'
+    ];
+}
