e51a3e7875163d3c8fde364878997d036933a493
Regis Zanandrea <regiszanandrea@gmail.com>
2020-05-30 15:00:34 -0300

    add base class test

---
+++ tests/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$rootPath = __DIR__ . '/..';
+
+$dotenv = Dotenv\Dotenv::createImmutable($rootPath);
+$dotenv->load();
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+
+require "{$rootPath}/bootstrap.php";
+
+return $app;
